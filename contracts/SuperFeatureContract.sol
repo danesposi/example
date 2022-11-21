4d2f144e4ef0eaa423dfe351a358cf276ebcadb3
danesposi <espdaniel1999@gmail.com>
2022-11-21 16:07:47 -0500

    rename contract

--- contracts/SuperFeatureContract.sol
+++ contracts/SuperFeatureContract.sol
@@ -4,7 +4,7 @@ pragma solidity >= 0.8.9;
 
 // FEATURE 1: Cambiar TemplateContract -> FeatureContract (CHECK :) ). 
 
-contract FeatureContract {
+contract SuperFeatureContract {
 
     constructor() {}
 
